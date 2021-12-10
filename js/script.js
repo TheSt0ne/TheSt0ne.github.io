ac201f08849c8dc0620f5e7fccf3e5d18cb99de6
piyushmulatkar <piyushmulatkar11@gmail.com>
2021-12-10 23:24:54 +0530

    Some more features

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 const toggleBackground = document.getElementById('nav'); 
+const toggleMenu = document.getElementById('static-menu');
 
 function changeBG() {
   if (document.body.scrollTop > 500|| document.documentElement.scrollTop > 500) {
     toggleBackground.className = "toggleBackground";
   } else {
     toggleBackground.className = "";
+  }
+}
+function myFunction(x) {
+  x.classList.toggle("change");
+  if(toggleMenu.hidden==true){
+  	toggleMenu.hidden=false;
+  }
+  else{
+  	toggleMenu.hidden=true;
   }
 }
 window.addEventListener('scroll',() => {
